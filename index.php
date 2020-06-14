bcb8c2b28cda9a57f3117fbf13a13f0881aed6b5
sgopinadhan <57213279+sgopinadhan@users.noreply.github.com>
2020-06-14 16:44:13 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php   ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Home</title>
+<meta content="utf-8">
+<meta viewport="width=device-width, initial-scale=1.0">
+<link  href="css/mystyle.css" rel="stylesheet"/>
+<link  href="css/animations.css" rel="stylesheet"/>
+<script type="text/javascript" src="js/myjs.js"></script>
+</head>
+<body>
+<div class="wrapper">
+  <span></span>
+    <span></span>
+      <span></span>
+        <span></span>
+          <span></span>
+            <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+<div class="pagebox">
+<?php include("leftnav.htm"); ?>
+
+<div class="rightmaincolumn" >
+
+<div class="home" id="home">
+<h1>Hi! <br> I'm Sruthy Gopinadhan. </h1>
+<p>A web developer from India, who is currently living in Dubai. I'm a well organised person who enjoys coding and reading ! </p>
+   <a href="cv/sruthyresume.pdf" download>
+   <p class="cvdownload">Download Resume </p></a>  
+</div>
+
+<div class="skills" id="skills" style="display: none;">
+  <h1>My Skills </h1>
+  <ul class="skillsList">
+  <li>PHP</li>
+  <li>MySql</li>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Javascript</li>
+  <li>Ajax</li>
+  <li>Joomla</li>
+  <li>Wordpress</li>
+  <li>Photoshop</li>
+</ul>
+</div>
+
+<div id="education" style="display: none;">
+  <h1>My Education </h1>
+  <ul class="education">
+    <li>MBA from University of Sunderland, UK [2012]</li>
+  <li>BTEC Level 7 Extended Diploma in Strategic Management and Leadership from EThames Graduate School, UK [2011]</li>
+  <li>MSc Computer Science from Mahatma Gandhi University, India [2008]</li>
+   <li>Bachelor of Computer Applications from Mahatma Gandhi University, India [2006]</li>
+</ul>
+</div>
+<div id="experience" style="display: none;">
+  <h1>My Experience </h1>
+   <ul class="experience">
+  <li>Web Developer at Monarch Sports, UK [November 2010 to June 2018]  <cite>Responsible for design, development, maintenance, content management, domain
+registration and hosting of company websites.</cite></li>
+  <li>IT Support Officer and Student Coordinator (Part Time) at Abis Resources, UK [Jan 2011 to June 2012]<cite> Mainly responsible for IT support, website and E-Learning management, Training staff and faculty, general administration,reports and file keeping.</cite></li>
+  <li> Web programmer at Vanilla Networks Pvt Ltd, India [March 2009 to August 2010] <cite>Coordinating and communicating with clients and web designers to plan & execute web development projects from concept through completion, utilizing a variety of web development tools and technologies along with content management and maintenance once they are up and running.</cite></li>
+  
+  <li> Java programmer Trainee at Cyber Prizm Ltd, India [October 2008 to March 2009] <cite>Responsible for software development and assisting students in technology assisted learning.</cite></li>
+
+</ul>
+</div>
+<div id="contact"  style="display: none;">
+  <h1>Contact me </h1>
+  <p>0552146497</p>
+  <p>sruthygopinath@gmail.com</p>
+</div>
+</div>
+
+</div>
+</div>
+</body>
+</html>
